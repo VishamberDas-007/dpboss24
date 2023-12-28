8e2b0c00fadc6f2f1ee05675b3c87b422ac7a368
VishamberDas-007 <vishamberbdas@gmail.com>
2023-12-28 07:31:06 +0530

    feat(satta): add middleware

---
+++ src/middlewares/sattaAuth.middleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express'
+import AppError from '../utils/AppError'
+import { GENERAL_E_0007, GENERAL_E_0013 } from '../config/responseCodes/general'
+import { TAccessToken } from '../types/global.types'
+import catchAsync from '../utils/catchAsync'
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../config/const'
+
+export const isSattaAdmin = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
+        if (!req.headers.authorization) throw new AppError(GENERAL_E_0013)
+
+        const token = req.headers.authorization.split(' ')[1]
+
+        const decodeTokenDetails = <TAccessToken>jwt.verify(token, JWT_SECRET)
+
+        const { email, isVerified } = decodeTokenDetails
+
+        if (!(email && isVerified)) throw new AppError(GENERAL_E_0007)
+
+        req.user = decodeTokenDetails
+
+        next()
+    }
+)
