28adddfb402eafa231b8b6c4171dc743466f3246
VishamberDas-007 <vishamberbdas@gmail.com>
2024-02-03 23:03:53 +0530

    build(satta)

--- dist/validations/satta.validator.js
+++ dist/validations/satta.validator.js
@@ -3,10 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.insertSattaTicketValueValidator = void 0;
+exports.sattaIdValidator = exports.insertSattaTicketValueValidator = void 0;
 const joi_1 = __importDefault(require("joi"));
 exports.insertSattaTicketValueValidator = joi_1.default.object({
     ticketValue: joi_1.default.string().max(5).required(),
     schedule: joi_1.default.string().required(),
-    createdAt: joi_1.default.date().required(),
+    createdAt: joi_1.default.string().required(),
+});
+exports.sattaIdValidator = joi_1.default.object({
+    sattaId: joi_1.default.string().required(),
 });
